f46749b659b0457afb3d30aca6f48dcdfa3af4eb
Md. Jamal Uddin <jamal@shellbeehaken.com>
2025-05-16 13:06:43 +0600

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | App Dashboard",
+    default: "App Dashboard",
+  },
+  description: "A business dashboard for managing invoices and customers.",
+  metadataBase: new URL("https://nextjs-dashboard-eta-ten-11.vercel.app/"),
+};
 
 export default function RootLayout({
   children,
